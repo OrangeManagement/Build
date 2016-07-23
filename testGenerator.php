6cb35b68c70d8dce9c70dde8fd819e1d3bae1d53
Dennis Eichhorn <spl1nes.com@googlemail.com>
2016-07-23 22:49:46 +0200

    Creating test templates

--- testGenerator.php
+++ testGenerator.php
@@ -25,51 +25,99 @@ function listFolderFiles($dir, $extension)
 }
 
 // PHP tests
-$base  = __DIR__ . '/../phpOMS';
-$files = listFolderFiles($base, '.php');
+$base     = __DIR__ . '/../phpOMS';
+$files    = listFolderFiles($base, '.php');
 $testBase = __DIR__ . '/../Tests/PHPUnit/Framework';
 
 foreach ($files as $file) {
-    $file   = str_replace($base, '', $file);
-    $subdir = trim($file, '/');
-    $split = explode('.', $file);
-    $testPath = $testBase . '/'. $split[0] . 'Test.' . $split[1];
+    $file     = str_replace($base, '', $file);
+    $subdir   = trim($file, '/');
+    $split    = explode('.', $file);
+    $testPath = $testBase . '/' . $split[0] . 'Test.' . $split[1];
 
-    if (strpos($subdir, 'Status.php') === false
-        && strpos($subdir, 'Type.php') === false
-        && strpos($subdir, 'Status') === false
-        && strpos($subdir, 'Enum') === false
-        && strpos($subdir, 'Null') === false
-        && strpos($subdir, 'Interface') === false
-        && strpos($subdir, 'Abstract') === false) {
-        if(!file_exists($testPath)) {
+    if (stripos($subdir, 'Status.php') === false
+        && stripos($subdir, 'Type.php') === false
+        && stripos($subdir, 'Status') === false
+        && stripos($subdir, 'Enum') === false
+        && stripos($subdir, 'Null') === false
+        && stripos($subdir, 'Interface') === false
+        && stripos($subdir, 'Abstract') === false
+    ) {
+        if (!file_exists($testPath)) {
+            $namespace = str_replace('/', '\\', $split[0]);
+            $namespace = explode('\\', $namespace);
+            $classname = $namespace[count($namespace) - 1] . 'Test';
+            unset($namespace[count($namespace) - 1]);
+            $namespace  = trim('Tests\PHPUnit\Framework\\' . trim(implode('\\', $namespace), '\\'), '\\');
+            $autoloader = str_repeat('/..', count(explode('\\', $namespace)));
+
             mkdir(dirname($testPath), 0777, true);
-            file_put_contents($testPath, '<?php');
+            file_put_contents($testPath,
+                '<?php' . PHP_EOL
+                . '/**' . PHP_EOL
+                . ' * Orange Management' . PHP_EOL
+                . ' *' . PHP_EOL
+                . ' * PHP Version 7.0' . PHP_EOL
+                . ' *' . PHP_EOL
+                . ' * @category   TBD' . PHP_EOL
+                . ' * @package    TBD' . PHP_EOL
+                . ' * @author     OMS Development Team <dev@oms.com>' . PHP_EOL
+                . ' * @author     Dennis Eichhorn <d.eichhorn@oms.com>' . PHP_EOL
+                . ' * @copyright  Dennis Eichhorn' . PHP_EOL
+                . ' * @license    OMS License 1.0' . PHP_EOL
+                . ' * @version    1.0.0' . PHP_EOL
+                . ' * @link       http://orange-management.com' . PHP_EOL
+                . ' */' . PHP_EOL
+                . '' . PHP_EOL
+                . 'namespace ' . $namespace . ';' . PHP_EOL
+                . '' . PHP_EOL
+                . 'require_once __DIR__ . \'' . $autoloader . '/phpOMS/Autoloader.php\';' . PHP_EOL
+                . '' . PHP_EOL
+                . 'class ' . $classname . ' extends \PHPUnit_Framework_TestCase' . PHP_EOL
+                . '{' . PHP_EOL
+                . '}' . PHP_EOL
+            );
         }
     }
 }
 
 // JS tests
-$base  = __DIR__ . '/../jsOMS';
-$files = listFolderFiles($base, '.js');
+$base     = __DIR__ . '/../jsOMS';
+$files    = listFolderFiles($base, '.js');
 $testBase = __DIR__ . '/../Tests/JS/Framework';
 
 foreach ($files as $file) {
-    $file   = str_replace($base, '', $file);
-    $subdir = trim($file, '/');
-    $split = explode('.', $file);
-    $testPath = $testBase . '/'. $split[0] . 'Test.' . $split[1];
+    $file     = str_replace($base, '', $file);
+    $subdir   = trim($file, '/');
+    $split    = explode('.', $file);
+    $testPath = $testBase . '/' . $split[0] . 'Test.' . $split[1];
 
-    if (strpos($subdir, 'Status.php') === false
-        && strpos($subdir, 'Type.php') === false
-        && strpos($subdir, 'Status') === false
-        && strpos($subdir, 'Enum') === false
-        && strpos($subdir, 'Null') === false
-        && strpos($subdir, 'Interface') === false
-        && strpos($subdir, 'Abstract') === false) {
-        if(!file_exists($testPath)) {
+    if (stripos($subdir, 'Status.php') === false
+        && stripos($subdir, 'Type.php') === false
+        && stripos($subdir, 'Status') === false
+        && stripos($subdir, 'Enum') === false
+        && stripos($subdir, 'Null') === false
+        && stripos($subdir, 'Interface') === false
+        && stripos($subdir, 'Abstract') === false
+    ) {
+        if (!file_exists($testPath)) {
+            $name = explode('/', $split[0]);
+
             mkdir(dirname($testPath), 0777, true);
-            file_put_contents($testPath, '');
+            file_put_contents($testPath,
+                'describe(\'' . $name[count($name) - 1] . 'Test\', function ()' . PHP_EOL
+                . '{' . PHP_EOL
+                . '    "use strict";' . PHP_EOL
+                . '' . PHP_EOL
+                . '    beforeEach(function ()' . PHP_EOL
+                . '    {' . PHP_EOL
+                . '    });' . PHP_EOL
+                . '' . PHP_EOL
+                . '    afterEach(function ()' . PHP_EOL
+                . '    {' . PHP_EOL
+                . '    });' . PHP_EOL
+                . '}' . PHP_EOL
+            );
         }
     }
 }
