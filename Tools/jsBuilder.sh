92079d16311141003a09dcfe718cbd7b815e9802
Dennis Eichhorn <spl1nes.com@googlemail.com>
2017-07-28 21:50:13 +0200

    Test own optimization

--- Tools/jsBuilder.sh
+++ Tools/jsBuilder.sh
@@ -7,17 +7,20 @@ do
     echo "" >> ${OUT}
 done
 
+# Remove single line comments
+sed -i -e '/\/\/.*//g' ${OUT}
 # Remove spaces at end of line
 sed -i -e 's/[[:blank:]]*$//g' ${OUT}
 
 # Minimize
-java -jar ${TOOLS_PATH}/closure-compiler* --compilation_level SIMPLE_OPTIMIZATIONS --js ${OUT} --js_output_file ${OUT}.min
-
-rm ${OUT}
-mv ${OUT}.min ${OUT}
+#java -jar ${TOOLS_PATH}/closure-compiler* --compilation_level SIMPLE_OPTIMIZATIONS --js ${OUT} --js_output_file ${OUT}.min
+#rm ${OUT}
+#mv ${OUT}.min ${OUT}
 
 # Make single line
 sed -i -e ':a;N;$!ba;s/\n/ /g' ${OUT}
+# Remove multiline comments
+sed -i -e '/\/\*/,/*\//g' ${OUT}
 # Remove multiple spaces
 sed -i -e 's/  */ /g' ${OUT}
 # Remove double js initialization
