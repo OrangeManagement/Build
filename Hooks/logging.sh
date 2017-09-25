8862eb7d0f099135710427d2ef7cddef92f1c014
Dennis Eichhorn <spl1nes.com@googlemail.com>
2017-09-25 13:13:21 +0200

    Update logging.sh

--- Hooks/logging.sh
+++ Hooks/logging.sh
@@ -11,7 +11,7 @@ git diff --cached --name-only | while read FILE; do
 if [[ "$FILE" =~ ^.+(php|js)$ ]]; then
     RESULT=$(grep "console.log(" "$FILE")
     if [ ! -z $RESULT ]; then
-      echo -e "\e[1;33m\tWarning, the commit contains a call to var_dump(). Commit was not aborted, however.\e[0m" >&2
+      echo -e "\e[1;33m\tWarning, the commit contains a call to console.log(). Commit was not aborted, however.\e[0m" >&2
     fi
 fi
 done
