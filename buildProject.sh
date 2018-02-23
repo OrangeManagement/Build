f19be01aee6e1ca68069b2407b94494ae4f6e878
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-02-23 12:46:18 +0100

    Update buildProject.sh

--- buildProject.sh
+++ buildProject.sh
@@ -14,7 +14,7 @@ mysql -e 'create database oms;' -u ${DB_USER} -p${DB_PASSWORD}
 #echo "USE mysql;\nUPDATE user SET password=PASSWORD('123456') WHERE user='root';\nFLUSH PRIVILEGES;\n" | mysql -u root
 
 # Run inspection
-. inspect.sh
+. Inspection/inspect.sh
 
 # Build documentation
 php ${TOOLS_PATH}/documentor.phar -s ${ROOT_PATH}/phpOMS -d ${BASE_PATH}/Inspection/Test/Php/docblock -c ${INSPECTION_PATH}/Test/Php/coverage.xml -u ${INSPECTION_PATH}/Test/Php/junit_php.xml -b http://docs.orange-management.de
