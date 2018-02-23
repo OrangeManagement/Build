8c9a794d534c952bd141127f78bcd52d0fdd2971
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-02-23 12:43:42 +0100

    Update buildProject.sh

--- buildProject.sh
+++ buildProject.sh
@@ -3,62 +3,8 @@
 # Include config
 . config.sh
 
-# Previous cleanup
-rm -r -f ${ROOT_PATH}
-mkdir -p ${ROOT_PATH}
-
-rm -r -f ${INSPECTION_PATH}
-mkdir -p ${INSPECTION_PATH}
-
-# Create git repositories
-c=0;
-for i in "${GITHUB_URL[@]}"
-do
-    if [ "$c" -eq 0 ]
-    then
-        cd ${BASE_PATH}
-    fi
-
-    if [ "$c" -eq 1 ]
-    then
-        cd ${ROOT_PATH}
-    fi
-
-    git clone -b ${GIT_BRANCH} $i
-    c=$((c+1))
-done
-
-cd ${ROOT_PATH}
-git submodule update --init --recursive
-
-# Creating directories for inspection
-mkdir -p ${INSPECTION_PATH}/logs
-mkdir -p ${INSPECTION_PATH}/Framework/logs
-mkdir -p ${INSPECTION_PATH}/Framework/metrics
-mkdir -p ${INSPECTION_PATH}/Framework/pdepend
-mkdir -p ${INSPECTION_PATH}/Framework/phpcs
-mkdir -p ${INSPECTION_PATH}/Framework/phpcpd
-mkdir -p ${INSPECTION_PATH}/Framework/linting
-mkdir -p ${INSPECTION_PATH}/Framework/html
-
-mkdir -p ${INSPECTION_PATH}/Modules/logs
-mkdir -p ${INSPECTION_PATH}/Modules/metrics
-mkdir -p ${INSPECTION_PATH}/Modules/pdepend
-mkdir -p ${INSPECTION_PATH}/Modules/phpcs
-mkdir -p ${INSPECTION_PATH}/Modules/phpcpd
-mkdir -p ${INSPECTION_PATH}/Modules/linting
-mkdir -p ${INSPECTION_PATH}/Modules/html
-
-mkdir -p ${INSPECTION_PATH}/Framework
-mkdir -p ${INSPECTION_PATH}/Web
-mkdir -p ${INSPECTION_PATH}/Model
-mkdir -p ${INSPECTION_PATH}/Modules
-
-mkdir -p ${INSPECTION_PATH}/Test/Php
-mkdir -p ${INSPECTION_PATH}/Test/Js
-
-# Permission handling
-chmod -R 777 ${ROOT_PATH}
+# Clean setup
+. setup.sh
 
 cd ${ROOT_PATH}
 
@@ -68,7 +14,7 @@ mysql -e 'create database oms;' -u ${DB_USER} -p${DB_PASSWORD}
 #echo "USE mysql;\nUPDATE user SET password=PASSWORD('123456') WHERE user='root';\nFLUSH PRIVILEGES;\n" | mysql -u root
 
 # Run inspection
-. Inspection/inspect.sh
+. inspect.sh
 
 # Build documentation
 php ${TOOLS_PATH}/documentor.phar -s ${ROOT_PATH}/phpOMS -d ${BASE_PATH}/Inspection/Test/Php/docblock -c ${INSPECTION_PATH}/Test/Php/coverage.xml -u ${INSPECTION_PATH}/Test/Php/junit_php.xml -b http://docs.orange-management.de
