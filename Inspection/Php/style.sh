3d1058bb51bd5172e0fa7d70de2bcc0dc1afac15
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-03-04 18:33:45 +0100

    Fix phpcs/phpmd path

--- Inspection/Php/style.sh
+++ Inspection/Php/style.sh
@@ -2,5 +2,5 @@
 
 . ${BUILD_PATH}/config.sh
 
-php ${TOOLS_PATH}/phpcs.phar ${ROOT_PATH}/phpOMS --colors --standard="${BUILD_PATH}/phpcs.xml" -s --report=full
-php ${TOOLS_PATH}/phpmd.phar ${ROOT_PATH}/phpOMS text ${BUILD_PATH}/phpmd.xml --exclude *tests* --minimumpriority 1
+php ${TOOLS_PATH}/phpcs.phar ${ROOT_PATH}/phpOMS --standard="${BUILD_PATH}/Config/phpcs.xml" -s --report=full
+php ${TOOLS_PATH}/phpmd.phar ${ROOT_PATH}/phpOMS text ${BUILD_PATH}/Config/phpmd.xml --exclude *tests* --minimumpriority 1
