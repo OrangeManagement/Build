678415a944faf511827104561bea5b7f449e3f10
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-03-04 19:00:02 +0100

    Fix build

--- Inspection/Php/style.sh
+++ Inspection/Php/style.sh
@@ -2,5 +2,5 @@
 
 . ${BUILD_PATH}/config.sh
 
-php ${TOOLS_PATH}/phpcs.phar ${ROOT_PATH}/phpOMS --standard="${BUILD_PATH}/Config/phpcs.xml" -s --report=full
-php ${TOOLS_PATH}/phpmd.phar ${ROOT_PATH}/phpOMS text ${BUILD_PATH}/Config/phpmd.xml --exclude *tests* --minimumpriority 1
+php ${TOOLS_PATH}/phpcs.phar ${ROOT_PATH}/phpOMS --standard="${BUILD_PATH}/Config/phpcs.xml" -s --report=full --report-file=/Framework/phpcs.log
+#php ${TOOLS_PATH}/phpmd.phar ${ROOT_PATH}/phpOMS text ${BUILD_PATH}/Config/phpmd.xml --exclude *tests* --minimumpriority 1
