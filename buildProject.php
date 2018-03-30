de4eb598f14a94e1a360279db63c07ce961edc61
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-03-30 12:17:01 +0200

    Add response message

--- buildProject.php
+++ buildProject.php
@@ -5,4 +5,8 @@ $payload  = json_decode($postBody, true);
 
 if ($payload['repository']['name'] === 'Orange-Management') {
     shell_exec('./buildProject.sh > /dev/null 2>/dev/null &');
+
+    echo 'Installing';
+} else {
+    echo 'Invalid payload';
 }
