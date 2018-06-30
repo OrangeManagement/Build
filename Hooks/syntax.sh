a9864baae64ff6f443562da03ea18265753cc8bf
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-06-30 22:59:52 +0200

    Implement shell linting

--- Hooks/syntax.sh
+++ Hooks/syntax.sh
@@ -2,9 +2,8 @@
 
 git diff --cached --name-only | while read FILE; do
 
-if [[ "$FILE" =~ ^.+(php|inc|module|install|test)$ ]]; then
+if [[ "$FILE" =~ ^.+(php)$ ]]; then
     if [[ -f $FILE ]]; then
-    
         # php lint
         php -l "$FILE" 1> /dev/null
         if [ $? -ne 0 ]; then
@@ -23,6 +22,17 @@ if [[ "$FILE" =~ ^.+(php|inc|module|install|test)$ ]]; then
         ${rootpath}/vendor/bin/phpmd $FILE text ${rootpath}/Build/Config/phpmd.xml --exclude *tests* --minimumpriority 1
         if [ $? -ne 0 ]; then
             echo -e "\e[1;31m\tMess Detector error.\e[0m" >&2
+            exit 1
+        fi
+    fi
+fi
+
+if [[ "$FILE" =~ ^.+(sh)$ ]]; then
+    if [[ -f $FILE ]]; then
+        # sh lint
+        bash -n "$FILE" 1> /dev/null
+        if [ $? -ne 0 ]; then
+            echo -e "\e[1;31m\tBash linting error.\e[0m" >&2
             exit 1
         fi
     fi
