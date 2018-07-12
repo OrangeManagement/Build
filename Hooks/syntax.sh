62d7423d96f524553d839657907f2f9cce6d5cfa
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-07-12 20:01:39 +0200

    Extend commit hook with static tests

--- Hooks/syntax.sh
+++ Hooks/syntax.sh
@@ -24,6 +24,18 @@ if [[ "$FILE" =~ ^.+(php)$ ]]; then
             echo -e "\e[1;31m\tMess Detector error.\e[0m" >&2
             exit 1
         fi
+
+        # phan
+        ${rootpath}/vendor/bin/phan -k ${rootpath}/Build/Config/phan.php -f $FILE
+        if [ $? -ne 0 ]; then
+            echo -e "\e[1;31m\tPhan warning.\e[0m" >&2
+        fi
+
+        # phpstan
+        php -d memory_limit=4G ${rootpath}/vendor/bin/phpstan analyse --autoload-file=${rootpath}/phpOMS/Autoloader.php -l 7 -c ${rootpath}/Build/Config/phpstan.neon $FILE
+        if [ $? -ne 0 ]; then
+            echo -e "\e[1;31m\tPhp stan warning.\e[0m" >&2
+        fi
     fi
 fi
 
