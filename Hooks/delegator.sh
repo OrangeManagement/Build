9dab7232f3885ce801fb59c4c7ddeb4afb4b1a45
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-07-17 22:31:21 +0200

    Ignore deleted files

--- Hooks/delegator.sh
+++ Hooks/delegator.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+git diff --cached --name-only | while read FILE; do
+if [[ ! -f "$FILE" ]]; then
+    exit 0
+fi
+done
+
 . ${rootpath}/Build/Hooks/logging.sh
 . ${rootpath}/Build/Hooks/syntax.sh
 . ${rootpath}/Build/Hooks/filename.sh
