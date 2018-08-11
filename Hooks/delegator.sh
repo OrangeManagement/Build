bd2226e822e056049e113bc526056885497d9976
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-08-11 12:16:37 +0200

    Test

--- Hooks/delegator.sh
+++ Hooks/delegator.sh
@@ -63,7 +63,7 @@ git diff --cached --name-only | while read FILE; do
 
     if [[ "$FILE" =~ ^.+(sh|js|php|json|css)$ ]]; then
         GEN_SYNTAX=$(hasInvalidBasicSyntax "$FILE")
-        echo GEN_SYNTAX
+        echo $GEN_SYNTAX
 
         if [[ $GEN_SYNTAX = 1 ]]; then
             echo -e "\e[1;31m\tFound whitespace at end of line in $FILE.\e[0m" >&2
