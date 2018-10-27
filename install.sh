bd5bcb0951e06e352f8a47915128ee6f75665844
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-10-27 20:41:12 +0200

    add info how to set password

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ apt-get update
 
 apt-get install git php7.2 php7.2-cli php7.2-common php7.2-mysql php7.2-pgsql php7.2-xdebug php7.2-json php7.2-opcache php7.2-pdo php7.2-sqlite php7.2-mbstring php7.2-curl php7.2-imap php7.2-bcmath php7.2-zip php7.2-dom php7.2-xml php7.2-phar php7.2-gd php7.2-dev php-pear apache2 mysql-server postgresql postgresql-contrib
 
+# ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '';
+
 update-rc.d postgresql enable
 
 a2enmod rewrite
