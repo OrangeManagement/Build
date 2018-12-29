ba618272e6d24127fc1d0d435544da1594982f1e
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-12-29 21:50:21 +0100

    Improve install commands

--- install.sh
+++ install.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+# Debian
+#wget -q https://packages.sury.org/php/apt.gpg -O- | sudo apt-key add -
+#echo "deb https://packages.sury.org/php/ stretch main" | sudo tee /etc/apt/sources.list.d/php.list
+
 apt-get update
 
 apt-get install npm git php7.2 php7.2-cli php7.2-common php7.2-mysql php7.2-pgsql php7.2-xdebug php7.2-json php7.2-opcache php7.2-pdo php7.2-sqlite php7.2-mbstring php7.2-curl php7.2-imap php7.2-bcmath php7.2-zip php7.2-dom php7.2-xml php7.2-phar php7.2-gd php7.2-dev php-pear apache2 mysql-server postgresql postgresql-contrib
 
+# USE mysql;
+# mysql < 5.7
+# UPDATE user SET plugin='mysql_native_password' WHERE User='root';
+# FLUSH PRIVILEGES;
+# exit;
+# else mysql >= 5.7
 # ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '';
 
 update-rc.d postgresql enable
@@ -16,39 +26,12 @@ pecl install ast
 echo "extension=ast.so" | tee /etc/php/7.2/mods-available/ast.ini
 phpenmod ast
 
-systemctl restart apache2
-
 # Install redis
-apt-get install apt-get install build-essential tcl
-curl -O http://download.redis.io/redis-stable.tar.gz
-tar xzvf redis-stable.tar.gz
-cd redis-stable
-make
-make test
-make install
-mkdir /etc/redis
-cp /tmp/redis-stable/redis.conf /etc/redis
-nano /etc/redis/redis.conf
+sudo apt install redis-server
+# /etc/redis/...conifg
 # supervised systemd
 # dir /var/lib/redis
-nano /etc/systemd/system/redis.service
-echo "[Unit]" >> /etc/systemd/system/redis.service
-echo "Description=Redis In-Memory Data Store" >> /etc/systemd/system/redis.service
-echo "After=network.target" >> /etc/systemd/system/redis.service
-echo "[Service]" >> /etc/systemd/system/redis.service
-echo "User=redis" >> /etc/systemd/system/redis.service
-echo "Group=redis" >> /etc/systemd/system/redis.service
-echo "ExecStart=/usr/local/bin/redis-server /etc/redis/redis.conf" >> /etc/systemd/system/redis.service
-echo "ExecStop=/usr/local/bin/redis-cli shutdown" >> /etc/systemd/system/redis.service
-echo "Restart=always" >> /etc/systemd/system/redis.service
-echo "[Install]" >> /etc/systemd/system/redis.service
-echo "WantedBy=multi-user.target" >> /etc/systemd/system/redis.service
-adduser --system --group --no-create-home redis
-mkdir /var/lib/redis
-chown redis:redis /var/lib/redis
-chmod 770 /var/lib/redis
-systemctl start redis
-systemctl enable redis
+systemctl restart redis
 
 pecl install redis
 echo "extension=redis.so" | tee /etc/php/7.2/mods-available/redis.ini
@@ -70,3 +53,5 @@ sudo useradd -d /home/testuser -g mail -u 1001 -s /bin/bash testuser
 
 # npm
 npm install -D jasmine jasmine-node istanbul jasmine-console-reporter supertest jasmine-supertest
+
+systemctl restart apache2
