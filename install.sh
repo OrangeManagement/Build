ae1ec5f196504a5f7b2083d26e1e5cd8ff993c0a
Dennis Eichhorn <spl1nes.com@googlemail.com>
2019-04-09 19:50:40 +0200

    Update to php7.3

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ usermod -aG sudo test
 
 apt-get update
 
-apt-get install npm git php7.2 php7.2-cli php7.2-common php7.2-mysql php7.2-pgsql php7.2-xdebug php7.2-json php7.2-opcache php7.2-pdo php7.2-sqlite php7.2-mbstring php7.2-curl php7.2-imap php7.2-bcmath php7.2-zip php7.2-dom php7.2-xml php7.2-phar php7.2-gd php7.2-dev php-pear apache2 mysql-server postgresql postgresql-contrib
+apt-get install npm git php7.3 php7.3-cli php7.3-common php7.3-mysql php7.3-pgsql php7.3-xdebug php7.3-json php7.3-opcache php7.3-pdo php7.3-sqlite php7.3-mbstring php7.3-curl php7.3-imap php7.3-bcmath php7.3-zip php7.3-dom php7.3-xml php7.3-phar php7.3-gd php7.3-dev php-pear apache2 mysql-server postgresql postgresql-contrib
 
 # USE mysql;
 # mysql < 5.7
@@ -35,7 +35,7 @@ a2enmod headers
 
 pecl install ast
 
-echo "extension=ast.so" | tee /etc/php/7.2/mods-available/ast.ini
+echo "extension=ast.so" | tee /etc/php/7.3/mods-available/ast.ini
 phpenmod ast
 
 # Install redis
@@ -46,14 +46,14 @@ sudo apt install redis-server
 systemctl restart redis
 
 pecl install redis
-echo "extension=redis.so" | tee /etc/php/7.2/mods-available/redis.ini
+echo "extension=redis.so" | tee /etc/php/7.3/mods-available/redis.ini
 phpenmod redis
 
 # Install memcached
 apt-get install memcached libmemcached-dev libmemcached-tools
 systemctl restart memcached
 pecl install memcached
-echo "extension=memcached.so" | tee /etc/php/7.2/mods-available/memcached.ini
+echo "extension=memcached.so" | tee /etc/php/7.3/mods-available/memcached.ini
 phpenmod memcached
 
 # Install email server for testing
