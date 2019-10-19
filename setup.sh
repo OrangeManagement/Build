1587a16f25de6f96ea6d705eb5f73135c18e29ea
Dennis Eichhorn <spl1nes.com@googlemail.com>
2019-10-19 14:11:16 +0200

    Update tesreport version

--- setup.sh
+++ setup.sh
@@ -110,7 +110,7 @@ wget --tries=2 -nc https://github.com/phan/phan/releases/download/0.12.5/phan.ph
 wget --tries=2 -nc https://github.com/FriendsOfPHP/PHP-CS-Fixer/releases/download/v2.13.1/php-cs-fixer.phar
 wget --tries=2 -nc https://github.com/jasmine/jasmine/releases/download/v3.1.0/jasmine-standalone-3.1.0.zip
 wget --tries=2 -nc https://github.com/Orange-Management/Documentor/releases/download/v1.1.1/documentor.phar
-wget --tries=2 -nc https://github.com/Orange-Management/TestReportGenerator/releases/download/1.1.0-rc1/testreportgenerator.phar
+wget --tries=2 -nc https://github.com/Orange-Management/TestReportGenerator/releases/download/1.1.0-rc2/testreportgenerator.phar
 
 unzip -n -j jasmine-standalone-3.1.0.zip -d ${ROOT_PATH}/jsOMS/tests
 tar -zxvf compiler-latest.tar.gz
