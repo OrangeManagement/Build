5d3cee66077c5d89ee84a1c4477939e5dc561d9b
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-02-25 22:46:48 +0100

    update phpstan version

--- Helper/testreport.sh
+++ Helper/testreport.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-mkdir Build/test
+mkdir -p Build/test
 
 php phpcs.phar ./ --standard="Build/Config/phpcs.xml"
-php phpstan.phar analyse --autoload-file=phpOMS/Autoloader.php -l 7 -c Build/Config/phpstan.neon ./
+php phpstan.phar analyse --autoload-file=phpOMS/Autoloader.php -l 8 -c Build/Config/phpstan.neon ./
 
 php phpcs.phar ./ --standard="Build/Config/phpcs.xml" -s --report-junit=Build/test/junit_phpcs.xml
-php phpstan.phar analyse --autoload-file=phpOMS/Autoloader.php -l 7 -c Build/Config/phpstan.neon --error-format=prettyJson ./ > Build/test/phpstan.json
+php phpstan.phar analyse --autoload-file=phpOMS/Autoloader.php -l 8 -c Build/Config/phpstan.neon --error-format=prettyJson ./ > Build/test/phpstan.json
 
 # Remove empty lines and lines with warnings which corrupt the json format
 sed -i '/^$/d' Build/test/phpstan.json
