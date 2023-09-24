7322bf89d8eb08479021ea1b8d546f885d4e3066
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-09-24 21:47:26 +0000

    fix autoloading for modules

--- Config/phpstan_autoloader.php
+++ Config/phpstan_autoloader.php
@@ -1,13 +1,21 @@
 <?php declare(strict_types=1);
 
-if (\is_file(__DIR__ . '/../../phpOMS/Autoloader.php')) {
+if (\is_file(__DIR__ . '/../../tests/Autoloader.php')) {
+    require_once __DIR__ . '/../../tests/Autoloader.php';
+} elseif (\is_file(__DIR__ . '/../../phpOMS/Autoloader.php')) {
     require_once __DIR__ . '/../../phpOMS/Autoloader.php';
 } else {
     require_once __DIR__ . '/../../Autoloader.php';
 }
 
-use phpOMS\Autoloader;
+use tests\Autoloader;
 
-Autoloader::addPath(__DIR__ . '/../../Resources');
-Autoloader::addPath(__DIR__ . '/../../Resources/tcpdf');
-Autoloader::addPath(__DIR__ . '/../../Resources/Stripe');
+if (\is_dir(__DIR__ . '/../../Karaka')) {
+    Autoloader::addPath(__DIR__ . '/../../Karaka/Resources');
+    Autoloader::addPath(__DIR__ . '/../../Karaka/Resources/tcpdf');
+    Autoloader::addPath(__DIR__ . '/../../Karaka/Resources/Stripe');
+} else {
+    Autoloader::addPath(__DIR__ . '/../../Resources');
+    Autoloader::addPath(__DIR__ . '/../../Resources/tcpdf');
+    Autoloader::addPath(__DIR__ . '/../../Resources/Stripe');
+}
