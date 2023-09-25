e6897dbe86eac192196c40569e1e938bfd134ed9
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-09-25 10:47:58 +0000

    fix rector

--- Config/rector.php
+++ Config/rector.php
@@ -62,6 +62,7 @@ return static function (RectorConfig $rectorConfig) : void {
     $rectorConfig->skip([
         $base . '/vendor',
         $base . '/Build',
+        $base . '/Resources',
         SimplifyEmptyCheckOnEmptyArrayRector::class,
         FlipTypeControlToUseExclusiveTypeRector::class,
         UnusedForeachValueToArrayKeysRector::class,
