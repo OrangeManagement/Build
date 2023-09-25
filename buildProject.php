737f5b78cdee94dbbeca9b8579f1e31873256875
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-09-25 15:54:41 +0000

    fix tests

--- buildProject.php
+++ buildProject.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 $postBody = $_POST['payload'] ?? '';
 $payload  = \json_decode($postBody, true);
 
-if (isset($payload['organization'], $payload['organization']['login']) && $payload['organization']['login'] === 'Karaka') {
+if (isset($payload['organization'], $payload['organization']['login'])
+    && $payload['organization']['login'] === 'Karaka'
+) {
     \shell_exec('./buildProject.sh > /dev/null 2>/dev/null &');
 
     echo 'Installing';
