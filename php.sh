0cf0e357f5784af0612c4aedd2497a3a6e598013
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-10-02 01:19:26 +0000

    fix paths

--- php.sh
+++ php.sh
@@ -11,8 +11,8 @@ fi
 
 REPO_PATH="$1"
 BASE_NAME=$(basename "$REPO_PATH" .git)
-INSPECTION_PATH="$2/$BASE_NAME"
-OUTPUT_PATH="$2/$BASE_NAME/build"
+INSPECTION_PATH="$(realpath "$2")/${BASE_NAME}"
+OUTPUT_PATH="$(realpath "$2")/${BASE_NAME}/build"
 
 rm -rf ${INSPECTION_PATH}
 
@@ -30,14 +30,16 @@ mkdir -p ${OUTPUT_PATH}/metrics
 
 git clone --recurse-submodules ${REPO_PATH} ${INSPECTION_PATH}
 git -C ${INSPECTION_PATH} checkout develop
-git -C ${INSPECTION_PATH} submodule foreach 'git checkout develop || true'
 git -C ${INSPECTION_PATH} pull
+git -C ${INSPECTION_PATH} submodule foreach 'git checkout develop || true'
+git -C ${INSPECTION_PATH} submodule foreach 'git pull || true'
 
 if [[ ${BASE_NAME} == *"oms-"* ]]; then
     git clone --recurse-submodules ${REPO_PATH} ${INSPECTION_PATH}/Karaka
     git -C ${INSPECTION_PATH}/Karaka checkout develop
-    git -C ${INSPECTION_PATH}/Karaka submodule foreach 'git checkout develop || true'
     git -C ${INSPECTION_PATH}/Karaka pull
+    git -C ${INSPECTION_PATH}/Karaka submodule foreach 'git checkout develop || true'
+    git -C ${INSPECTION_PATH}/Karaka submodule foreach 'git pull || true'
 fi
 
 # Run inspection
