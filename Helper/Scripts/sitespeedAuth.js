08bb46bff4d4c8526c0e31cb1e156c8495a4e592
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-10-20 13:08:55 +0000

    icon fixes, loading optimizations

--- Helper/Scripts/sitespeedAuth.js
+++ Helper/Scripts/sitespeedAuth.js
@@ -1,11 +1,12 @@
+// sitespeed.io /var/www/html/Karaka/Build/Helper/Scripts/sitespeedDemoUrls.txt -n 1 -b chrome --preScript /var/www/html/Karaka/Build/Helper/Scripts/sitespeedAuth.js --outputFolder /var/www/html/sitespeed
 module.exports = async function(context, commands)
 {
-    await commands.navigate('https://jingga.app');
+    await commands.navigate('http://192.168.178.38/en/backend');
 
-    await commands.addText.byId('admin', 'iName');
-    await commands.addText.byId('orange', 'iPassword');
+    // await commands.addText.byId('admin', 'iName');
+    // await commands.addText.byId('orange', 'iPassword');
 
     await commands.click.byIdAndWait('iLoginButton');
 
-    return commands.wait.byId('u-box', 3000);
+    return commands.wait.byId('u-box', 1000);
 };
