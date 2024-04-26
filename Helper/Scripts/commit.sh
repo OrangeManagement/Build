53515f3c1db9a7b28fdf183230fd92684db28929
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2024-04-26 22:02:22 +0000

    redirect fixes

---
+++ Helper/Scripts/commit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+MSG="$1"
+
+git submodule foreach git add .
+git submodule foreach "git commit -m '${MSG}' || true"
+git submodule foreach git push
+git add .
+git commit -m '${MSG}'
+git push
