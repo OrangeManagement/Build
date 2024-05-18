dd8febcace14e42d5fb49ff29e656fca69ea4eba
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2024-05-18 02:15:38 +0000

    fix tests

--- Helper/Scripts/commit.sh
+++ Helper/Scripts/commit.sh
@@ -3,8 +3,8 @@
 MSG="$1"
 
 git submodule foreach git add .
-git submodule foreach "git commit -m '${MSG}' || true"
+git submodule foreach "git commit -m \"${MSG}\" || true"
 git submodule foreach git push
 git add .
-git commit -m '${MSG}'
+git commit -m "${MSG}"
 git push
